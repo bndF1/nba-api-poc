db90bcd13cdcff92d86fdd41341ac4ed3a9284e8
bndF1 <jesbegar@gmail.com>
2019-07-29 08:06:38 +0200

    Added infinte scroll, but still need to check

--- src/app/features/players/players.component.ts
+++ src/app/features/players/players.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { flatMap, map, tap, toArray } from 'rxjs/operators';
 import { PlayersService } from '../../core';
-import { map, flatMap, tap, toArray } from 'rxjs/operators';
-import { combineLatest, of, concat, from } from 'rxjs';
-
 
 @Component({
   selector: 'app-players',
@@ -10,52 +8,64 @@ import { combineLatest, of, concat, from } from 'rxjs';
   styleUrls: ['./players.component.scss']
 })
 export class PlayersComponent implements OnInit {
-
   players$;
   ghosts = [];
-  pageSize;
+  pageSize = 25;
   currentPage = 0;
-  constructor(private playersService: PlayersService) { }
+  loading = false;
+  players = new Array();
+  constructor(private playersService: PlayersService) {}
 
   ngOnInit() {
     this.ghosts = new Array(25);
-    this.players$ = this.getPlayersByPage(this.currentPage);
-
+    // this.players$ = this.getPlayersByPage(this.currentPage);
   }
 
   getPlayerImg(player: any) {
-    return this.playersService.getPlayerImage(player.first_name, player.last_name);
+    return this.playersService.getPlayerImage(
+      player.first_name,
+      player.last_name
+    );
   }
 
   trackByFn(index, item) {
     return item.id;
   }
 
+  loadNext() {
+    if (this.loading) {
+      return;
+    }
+
+    this.loading = true;
+    this.ghosts = new Array(this.pageSize);
+
+    this.players$ = this.getPlayersByPage(this.currentPage).pipe(
+      map(result => {
+        this.players.push(...result);
+        return this.players;
+      }),
+      tap(() => {
+        this.loading = false;
+        this.currentPage++;
+      })
+    );
+  }
+
   getPlayersByPage(page) {
-    return this.playersService.getPlayers(page)
-      .pipe(
-        flatMap((player: { data }) =>
-          player.data
-        ),
-        flatMap((data: { picture }) => {
-          const img$ = this.getPlayerImg(data);
-          return img$.pipe(map((res) => {
+    return this.playersService.getPlayers(page).pipe(
+      flatMap((player: { data }) => player.data),
+      flatMap((data: { picture }) => {
+        const img$ = this.getPlayerImg(data);
+        return img$.pipe(
+          map(res => {
             data.picture = res;
             return data;
-          }));
-        }),
-        toArray(),
-        tap(() => this.ghosts = [])
-      );
+          })
+        );
+      }),
+      toArray(),
+      tap(() => (this.ghosts = []))
+    );
   }
-
-  loadNext() {
-    if (this.ghosts.length) { return }
-    this.ghosts = new Array(25);
-    this.players$ = this.getPlayersByPage(this.currentPage++);
-    //.pipe(map((result) => {
-    //   return [...result]
-    // }));
-  }
-
 }
