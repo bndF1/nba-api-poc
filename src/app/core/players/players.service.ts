311d83f63494950371ef14618101834c5b10b96e
bndF1 <jesbegar@gmail.com>
2019-10-03 09:36:09 +0200

    Changed the way we obtain players and its images in order to keep the order

--- src/app/core/players/players.service.ts
+++ src/app/core/players/players.service.ts
@@ -1,8 +1,13 @@
+import {
+  concatMap,
+  map,
+  switchMap,
+  toArray
+  } from 'rxjs/operators';
 import { Constants } from '../constants';
-import { flatMap, map, toArray } from 'rxjs/operators';
+import { from, Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { WindowRefService } from '../window/window-ref.service';
 
 @Injectable()
@@ -10,13 +15,14 @@ export class PlayersService {
   // USAGE: GET /players/:lastName/:firstName
   private PLAYER_IMG_URL = 'https://nba-players.herokuapp.com/players';
 
-  private _window: Window;
+  private _window;
   private itemsPerPage = 25;
 
   PLAYERS_API = '/players';
 
+  players$ = this.httpClient.get(`${Constants.BASE_API_URL + this.PLAYERS_API}`)
+    .pipe(map((result: ApiPlayersResponse<Player>) => result.data));
 
-  players$ = this.httpClient.get(`${Constants.BASE_API_URL + this.PLAYERS_API}`);
 
   constructor(
     private httpClient: HttpClient,
@@ -30,36 +36,43 @@ export class PlayersService {
   }
 
   public getPlayersByPage(page) {
-    return this.getPlayers(page).pipe(
-      flatMap((player: { data }) => player.data),
-      flatMap((data: { picture }) => {
-        const img$ = this.getPlayerImg(data);
-        return img$.pipe(
-          map(res => {
-            data.picture = res;
-            return data;
-          })
-        );
-      }),
-      toArray()
-    );
+    return this.getPlayers(page)
+      .pipe(
+        switchMap((players: Player[]) =>
+          from(players)
+            .pipe(
+              concatMap((player: Player) =>
+                this.getPlayerImg(player)
+                  .pipe(
+                    map((img: string) =>
+                      ({
+                        ...player,
+                        picture: img
+                      }) as Player
+                    )
+                  )
+              ),
+              toArray()
+            )
+        )
+      );
   }
 
   // ?page=0&per_page=25
-  private getPlayers(page): Observable<any> {
+  private getPlayers(page): Observable<Player[]> {
     const params = new HttpParams()
       .set('page', page)
       .set('per_page', this.itemsPerPage.toString());
-    return this.httpClient.get(`${Constants.BASE_API_URL + this.PLAYERS_API}`, {
-      params
-    });
+
+    return this.httpClient.get(`${Constants.BASE_API_URL + this.PLAYERS_API}`, { params })
+      .pipe(map((result: ApiPlayersResponse<Player>) => result.data));
   }
 
-  private getPlayerImg(player: any) {
+  private getPlayerImg(player: Player) {
     return this.getPlayerImage(player.first_name, player.last_name);
   }
 
-  public getPlayerImage(firstName, lastName): Observable<any> {
+  public getPlayerImage(firstName, lastName) {
     return this.httpClient
       .get(
         `${this.PLAYER_IMG_URL}` + '/' + `${lastName}` + '/' + `${firstName}`,
---
+++ src/app/shared/data/data.d.ts
@@ -0,0 +1,36 @@
+declare interface Player {
+    id: number;
+    first_name: string;
+    height_feet?: number;
+    height_inches?: number;
+    last_name: string,
+    position: string;
+    weight_pounds?: number;
+    picture?: string;
+    team: Team;
+}
+
+declare interface Team {
+    id: number;
+    abbreviation: string;
+    city: string;
+    conference: string;
+    division: string;
+    full_name: string;
+    name: string;
+}
+
+declare interface MetaInfo {
+    current_page: number;
+    next_page: number;
+    per_page: number;
+    total_count: number;
+    total_pages: number;
+}
+
+declare interface ApiPlayersResponse<T> {
+    data: Array<T>;
+    meta: MetaInfo;
+}
+
+
